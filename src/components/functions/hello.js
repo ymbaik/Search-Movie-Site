45069e410fb515b3e20ba13cb49794804c331537
ymbaik <ymbaik@empas.com>
2021-05-19 17:19:53 +0900

    Add Netlify functions

---
+++ src/components/functions/hello.js
@@ -0,0 +1,11 @@
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    // body: 'Hello World!'  //문자데이터만 할당가능
+    body: JSON.stringify({
+      name: 'YMBAIK',
+      age: 55,
+      email: 'ymb5890@gmail.com'
+    })
+  }
+}
