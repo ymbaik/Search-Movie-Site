b95c83d465cbc88df93b9d155574a361e9d59b0c
ymbaik <ymbaik@empas.com>
2021-05-26 06:26:32 +0900

    ch5-06 비동기 테스트

--- functions/tests/example.test.js
+++ functions/tests/example.test.js
@@ -1,5 +1,15 @@
-import { double } from './example'
+import { asyncFn } from './example'
+//done 매개변수는 비동기 테스트가 언제 종료되는지를 알려준다
+describe('비동기 태스트', () => {
+  //단순한 한줄의 키워드의 경우 콜백함수에서 중괄호{}와 return 키워드 생략가능하다
+  // test('resolves', () => {
+  //   return expect(asyncFn()).resolves.toBe('Done!')
+  // })
+  test('resolves', () => expect(asyncFn()).resolves.toBe('Done!'))
 
-test('첫 테스트', () => {
-  expect(123).toBe(123)
+  //단순한 비동기 프로그램 생성
+  test('async/await', async () => {
+    const res = await asyncFn()
+    expect(res).toBe('Done!')
+  }, 7000)
 })
